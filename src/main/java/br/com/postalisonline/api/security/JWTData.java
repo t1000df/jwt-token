31eef2011763ef5c3d808002170d56b76eb90c81
alessandro.lima <alessandro.lima@4quality.com.br>
2019-05-13 17:02:27 -0300

    Criacao da estrutura inicial.

---
+++ src/main/java/br/com/postalisonline/api/security/JWTData.java
@@ -0,0 +1,55 @@
+package br.com.postalisonline.api.security;
+
+import java.util.Date;
+import java.util.Map;
+
+public class JWTData {
+	
+	
+	private String id;
+	private String subject;
+	private String issuer;
+	private Date expiration;
+	
+	private Map<String,Object> claims;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getIssuer() {
+		return issuer;
+	}
+
+	public void setIssuer(String issuer) {
+		this.issuer = issuer;
+	}
+
+	public Date getExpiration() {
+		return expiration;
+	}
+
+	public void setExpiration(Date expiration) {
+		this.expiration = expiration;
+	}
+
+	public Map<String, Object> getClaims() {
+		return claims;
+	}
+
+	public void setClaims(Map<String, Object> claims) {
+		this.claims = claims;
+	}
+}
